84d34f3ebdfb808fc9f65b5ebc362e2c81a4eb04
sadia-mehjabin <jhidnicraft@gmail.com>
2022-10-24 23:45:23 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const courses = require('./fake-data/courses.json')
+
+app.get('/', (req, res) =>{
+    res.send('handicraft server is running')
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+
+app.listen(port, ()=>{
+    console.log(`handicraft is running on port ${port}`)
+})
